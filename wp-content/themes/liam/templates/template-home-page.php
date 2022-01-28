55347fe58e1ee9217ad52c200e9baebbc4bcebbf
Liam Lord <liam.lord@aceville.co.uk>
2022-01-28 10:17:27 +0000

    Adding WP Files

---
+++ wp-content/themes/liam/templates/template-home-page.php
@@ -0,0 +1,20 @@
+<?php /* Template Name: Home Page */
+
+get_header(); ?>
+
+	<main id="main" class="full-width site-main content-area" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+        
+            
+            <section class="full-width text-standard main-text" id="main-text-home">
+                <div class="content-width content-max-width-1200 pad-h-20-1299">
+                	<?php the_content(); ?>
+                </div>
+            </section>
+
+        <?php endwhile; // End of the loop. ?>
+
+    </main><!-- #main -->
+
+<?php get_footer(); ?>
